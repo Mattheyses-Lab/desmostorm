f842b626389c9ac32df905c915c4fb5ad0050e6e
will-f-dean-36 <will.f.dean.36@gmail.com>
2025-12-30 00:05:46 -0500

    Finalized outlier puncta suppression algorithm (for now)

--- +utils/suppressHotPuncta.m
+++ +utils/suppressHotPuncta.m
@@ -1,38 +1,43 @@
-function Iout = suppressHotPuncta(I, pHigh, dilateRadius, medWin)
+function Iout = suppressHotPuncta(I, PercentileThreshold, DilateRadius)
 %SUPPRESSHOTPUNCTA Mask extreme bright puncta and replace locally.
-% pHigh: percentile defining "hot" (e.g. 99.95)
-% dilateRadius: in pixels (e.g. 2 to 4)
-% medWin: median filter window (odd, e.g. 9 or 11)
+% PercentileThreshold: percentile defining "hot" (e.g. 99.95)
+% DilateRadius: in pixels (e.g. 2 to 4)
 
-    if nargin < 2 || isempty(pHigh),        pHigh = 99.99; end
-    if nargin < 3 || isempty(dilateRadius), dilateRadius = 7; end
-    if nargin < 4 || isempty(medWin),       medWin = 11; end
+    arguments
+        I
+        PercentileThreshold = 99.99
+        DilateRadius = 3
+    end
 
+    % convert to double
     I = im2double(I);
 
-    hi = prctile(I(:), pHigh);
+    % find pixels above specified threshold
+    hi = prctile(I(:), PercentileThreshold);
     mask = I >= hi;
 
     % opening to remove very small objects
     mask = imopen(mask,strel('disk',1,0));
 
     % Dilate to cover the full bright blob
-    se = strel('disk', dilateRadius, 0);
+    se = strel('disk', DilateRadius, 0);
     mask = imdilate(mask, se);
 
-    % --- method 1 ---
-    % set mask pixels to 0 in I
-    I(mask) = 0;
-    % Apply median filtering to smooth the image after masking
-    Imed = medfilt2(I, [medWin medWin]);
-    % replace masked pixels in input with median filtered pixels
-    I(mask) = Imed(mask);
+    % fill regions specified by mask
+    Iout = inpaintCoherent(I,mask,...
+        "SmoothingFactor",2,...
+        "Radius",3);
 
+    % create mask of dilated border around each region
+    borderMask = imdilate(mask,strel('disk',1,0)) & ~imerode(mask,strel('disk',1,0));
 
-    % % ---method 2 ---
-    % Iopen = imgaussfilt(I,25);
-    % % replace masked pixels in input with median filtered pixels
-    % I(mask) = Iopen(mask);
+    % median filter to smooth border
+    Imed = medfilt2(Iout,[3 3]);
 
-    Iout = rescale(I);
+    % fill the border pixels
+    Iout(borderMask) = Imed(borderMask);
+
+    % rescale output
+    Iout = rescale(Iout);
+
 end
