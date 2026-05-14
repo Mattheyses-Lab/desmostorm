c2cc0cebd1e389d501161221299c2c6c52f10d16
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-05-14 16:34:07 -0500

    Add root folder to search path on setup

--- +app/setupSearchPath.m
+++ +app/setupSearchPath.m
@@ -1,6 +1,9 @@
 function setupSearchPath()
 %SETUPSEARCHPATH Adds necessary folder to MATLAB search path
 
+% add project root to MATLAB search path
+addpath(app.Paths.root());
+
 % add external libraries to MATLAB search path
 addpath(genpath(app.Paths.external()));
 savepath();
