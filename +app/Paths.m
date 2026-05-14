50b4126c3b4644ecfad610d1ba16e4474fcf1b35
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-05-14 15:45:39 -0500

    Add 'external' to Paths helper

--- +app/Paths.m
+++ +app/Paths.m
@@ -7,25 +7,35 @@ classdef Paths
             here = fileparts(mfilename('fullpath'));
             r = fileparts(here);
         end
+
         function p = user(varargin)
             p = fullfile(app.Paths.root(), 'user', varargin{:});
         end
+
         function p = configDir()
             % per-user writable folder for settings.json
             d = fullfile(prefdir, char(app.Info.Name));
             if ~exist(d,'dir'), mkdir(d); end
             p = d;
         end
+
         function p = settingsFile()
             % full path to settings.json
             p = fullfile(app.Paths.configDir(), 'settings.json');
         end
+
         function p = assets()
             p = fullfile(app.Paths.root, 'assets');
         end
+
         function p = ml()
             p = fullfile(app.Paths.assets,'ml');
         end
+
+        function p = external()
+            p = fullfile(app.Paths.root(), 'external');
+        end
+
     end
 
 end
