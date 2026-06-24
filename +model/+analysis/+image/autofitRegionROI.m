bacb385cc8d668f628c677a15c92051595bcca9f
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-06-24 00:35:24 -0500

    Changed strategy for auto-fitting region ROI, still in development

--- +model/+analysis/+image/autofitRegionROI.m
+++ +model/+analysis/+image/autofitRegionROI.m
@@ -1,66 +1,107 @@
 function ROI = autofitRegionROI(I,config)
 
-%% TEST BELOW
-
 % --- preprocess image to improve fitting ---
-
 I = model.analysis.image.removeIsolatedPuncta(I);
-
-
-%% END TEST
 
 
 [H,W] = size(I, [1 2]);
 
 % starting ROI position
-Height = H / 2;
-Width = W / 2;
-CenterX = (W / 2) + 0.5;
-CenterY = (H / 2) + 0.5;
-RotationAngle = 0;
+ROI = struct();
+ROI.Height = H / 2;
+ROI.Width = W / 2;
+ROI.CenterX = (W / 2) + 0.5;
+ROI.CenterY = (H / 2) + 0.5;
+ROI.RotationAngle = 0;
 
 % --- find RotationAngle ---
 
-maxPeakDist = 0;
-
 thetas = -90:1:89;
 nPeaks = zeros(size(thetas));
+
+
+maxPeakDistTheta = 0;
+maxPeakDist = 0;
+
+
+% --- temp debug ui ---
+
+showPlots = false;
+
+if showPlots
+    f = uifigure("WindowStyle","alwaysontop",...
+        "OuterPosition",matlabx.ui.calibration.getCenteredFigOuterPosition(1000,500));
+    g = uigridlayout(f,[1,2],...
+        "ColumnWidth",{"1x","2x"},...
+        "RowHeight",{"1x"});
+    ax = matlabx.ui.widgets.ImageAxes(g,...
+        "ToolBelt",{'DrawRectangle'});
+    ax.Tools.DrawRectangle.RotationAngleMode = 'half-circle';
+    peaksPlot = widgets.PeaksPlotContainer(g);
+end
+
+
+% ---------------------
+
 
 % iterate to roughly estimate RotationAngle
 for i = 1:numel(thetas)
 
     theta = thetas(i);
 
+    I2 = I;
+
+    I2 = imdilate(I2,strel('line',W/10,theta));
+
     % compute linescan
-    linescanData = model.analysis.profile.measure2D(I,...
-        CenterX,...
-        CenterY,...
-        Width,...
-        Height,...
+    linescanData = model.analysis.profile.measure2D(I2,...
+        ROI.CenterX,...
+        ROI.CenterY,...
+        ROI.Width,...
+        ROI.Height,...
         theta,...
         'Interp','linear');
 
     % analyze peaks
     peaksData = model.analysis.PeaksData(linescanData.HeightProfile,linescanData.HeightDist,...
-    "MinPeakDistance",config.MinPeakDistance, ...
-    "MinPeakHeight",config.MinPeakHeight, ...
-    "PeakSmoothing",config.PeakSmoothing);
+        "MinPeakDistance",config.MinPeakDistance, ...
+        "MinPeakHeight",config.MinPeakHeight, ...
+        "PeakSmoothing",config.PeakSmoothing);
 
-    % % only consider scans with 2 peaks
-    % if peaksData.nPeaks ~= 2
-    %     continue
-    % end
-    % 
-    % % if peak distance greater than current max, set RotationAngle=theta
-    % if peaksData.PeakDistances(1) > maxPeakDist
-    %     RotationAngle = theta;
-    %     maxPeakDist = peaksData.PeakDistances;
-    % end
-
+    % method 1 - only consider scans with 2 peaks
     nPeaks(i) = peaksData.nPeaks;
+
+    if peaksData.hasCentralPeakPair
+        if peaksData.CentralPeakPairDistance > maxPeakDist
+            maxPeakDist = peaksData.CentralPeakPairDistance;
+            maxPeakDistTheta = theta;
+        end
+    end
+
+
+
+    % --- temp debug ui ---
+
+    if showPlots
+        ax.ImageData = matlabx.image.Image5D.fromComponents({I2},"Names",[sprintf("%i°",theta)]);
+    
+        rectROI = ROI;
+        rectROI.RotationAngle = theta;
+        ax.Tools.DrawRectangle.setROIPosition(rectROI);
+    
+        peaksPlot.Data = peaksData;
+    
+        drawnow
+        pause(0.05)
+    end
+    
+    
+    % ---------------------
+
 
 end
 
+% --- method 1 ---
 % find longest consecutive stretch of thetas that yield 2 peaks
 thetaIdxRange = matlabx.array.longestRunOfValue(nPeaks,2,"AllowWrap",true);
 
@@ -72,7 +113,11 @@ end
 
 % central theta of range becomes our RotationAngle
 centerThetaIdx = thetaIdxRange(ceil(numel(thetaIdxRange)/2));
-RotationAngle = thetas(centerThetaIdx);
+ROI.RotationAngle = thetas(centerThetaIdx);
+
+
+% % --- method 2 ---
+% ROI.RotationAngle = maxPeakDistTheta;
 
 
 
@@ -80,11 +125,11 @@ RotationAngle = thetas(centerThetaIdx);
 
 % recompute scan and peaks using best RotationAngle
 linescanData = model.analysis.profile.measure2D(I,...
-    CenterX,...
-    CenterY,...
-    Width,...
-    Height,...
-    RotationAngle,...
+    ROI.CenterX,...
+    ROI.CenterY,...
+    ROI.Width,...
+    ROI.Height,...
+    ROI.RotationAngle,...
     'Interp','linear');
 peaksData = model.analysis.PeaksData(linescanData.HeightProfile,linescanData.HeightDist,...
     "MinPeakDistance",config.MinPeakDistance, ...
@@ -106,7 +151,7 @@ peakIdxs = arrayfun(@(loc) find(X==loc,1),locs);
 p1 = peakIdxs(1);
 p2 = peakIdxs(2);
 
-% first local minimum to the left of peak 1
+% --- first local minimum to the left of peak 1 ---
 
 % look for 0s first
 leftIdx = find(Y(1:p1-1)==0, 1, 'last');
@@ -132,7 +177,7 @@ end
 
 
 
-% first local minimum to the right of peak 2
+% --- first local minimum to the right of peak 2 ---
 
 % look for 0s first
 rightIdx = find(Y(p2+1:end)==0, 1, 'first');
@@ -160,55 +205,96 @@ end
 
 
 % left min first
+dH_Top = X(1) - X(leftMinIdx);
+ROI = shiftTopEdge(dH_Top,ROI);
 
-% distance to adjust ROI height to trim from the "top"
-dH_Top = X(leftMinIdx) - X(1);
-
-% distance to shift center to account for changing height
-dC_Top = dH_Top / 2;
-
-% X and Y shifts, unsigned
-dX_Top = cosd(90-abs(RotationAngle))*dC_Top;
-dY_Top = sind(90-abs(RotationAngle))*dC_Top;
+% right min
+dH_Bot = X(rightMinIdx) - X(end);
+ROI = shiftBottomEdge(dH_Bot,ROI);
 
 
 
+
+% left min first
+
+% % distance to adjust ROI height to trim from the "top"
+% dH_Top = X(leftMinIdx) - X(1);
+% 
+% % distance to shift center to account for changing height
+% dC_Top = dH_Top / 2;
+% 
+% % X and Y shifts, unsigned
+% dX_Top = cosd(90-abs(RotationAngle))*dC_Top;
+% dY_Top = sind(90-abs(RotationAngle))*dC_Top;
 
 % right min
 
-% distance to adjust ROI height to trim from the "bottom"
-dH_Bot = X(end) - X(rightMinIdx);
+% % distance to adjust ROI height to trim from the "bottom"
+% dH_Bot = X(end) - X(rightMinIdx);
+% 
+% % distance to shift center to account for changing height
+% dC_Bot = dH_Bot / 2;
+% 
+% % X and Y shifts, unsigned
+% dX_Bot = cosd(90-abs(RotationAngle))*dC_Bot;
+% dY_Bot = sind(90-abs(RotationAngle))*dC_Bot;
+% 
+% % apply signs
+% if RotationAngle < 0
+%     dX_Top = -dX_Top;
+%     dY_Bot = -dY_Bot;
+% else
+%     dX_Bot = -dX_Bot;
+%     dY_Bot = -dY_Bot;
+% end
+% 
+% % Adjust the ROI dimensions based on the calculated shifts
+% Height = Height - dH_Top - dH_Bot;
+% CenterX = CenterX + (dX_Top + dX_Bot);
+% CenterY = CenterY + (dY_Top + dY_Bot);
 
-% distance to shift center to account for changing height
-dC_Bot = dH_Bot / 2;
-
-% X and Y shifts, unsigned
-dX_Bot = cosd(90-abs(RotationAngle))*dC_Bot;
-dY_Bot = sind(90-abs(RotationAngle))*dC_Bot;
-
-% apply signs
-if RotationAngle < 0
-    dX_Top = -dX_Top;
-    dY_Bot = -dY_Bot;
-else
-    dX_Bot = -dX_Bot;
-    dY_Bot = -dY_Bot;
-end
-
-% Adjust the ROI dimensions based on the calculated shifts
-Height = Height - dH_Top - dH_Bot;
-CenterX = CenterX + (dX_Top + dX_Bot);
-CenterY = CenterY + (dY_Top + dY_Bot);
-
-
-% collect output as struct
-ROI = struct(...
-    "CenterX",CenterX, ...
-    "CenterY",CenterY, ...
-    "Width",Width, ...
-    "Height", Height, ...
-    "RotationAngle", RotationAngle);
+% % collect output as struct
+% ROI = struct(...
+%     "CenterX",CenterX, ...
+%     "CenterY",CenterY, ...
+%     "Width",Width, ...
+%     "Height", Height, ...
+%     "RotationAngle", RotationAngle);
 
 % matlabx.struct.prettyPrint(ROI);
+
+
+
+    function ROIout = shiftTopEdge(d,ROI)
+        % distance to shift center to account for changing height
+        dC = d / 2;
+        % center X and Y shifts
+        dX = cosd(90 + ROI.RotationAngle)*dC;
+        dY = -sind(90 + ROI.RotationAngle)*dC;
+        % Adjust the ROI dimensions based on the calculated shifts
+        ROIout = ROI;
+        ROIout.Height = ROI.Height + d;
+        ROIout.CenterX = ROI.CenterX + dX;
+        ROIout.CenterY = ROI.CenterY + dY;
+    end
+
+
+
+    function ROIout = shiftBottomEdge(d,ROI)
+        % distance to shift center to account for changing height
+        dC = d / 2;
+        % center X and Y shifts
+        dX = -cosd(90 + ROI.RotationAngle)*dC;
+        dY = sind(90 + ROI.RotationAngle)*dC;
+        % Adjust the ROI dimensions based on the calculated shifts
+        ROIout = ROI;
+        ROIout.Height = ROI.Height + d;
+        ROIout.CenterX = ROI.CenterX + dX;
+        ROIout.CenterY = ROI.CenterY + dY;
+    end
+
+
+
+
 
 end
